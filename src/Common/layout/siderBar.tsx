2cba54b3582ec3a2b782760f04f276587e9603fc
NelsonJfsg <nelin09001@gmail.com>
2023-05-16 09:09:22 -0700

    ad

--- src/Common/layout/siderBar.tsx
+++ src/Common/layout/siderBar.tsx
@@ -1,257 +1,166 @@
-import { Category, Hail, Inventory, LocalShipping, Menu, MenuBook, Person, TurnedInNot } from "@mui/icons-material"
+import { Category, FireTruck, Google, Hail, Inventory, LocalShipping, Menu, MenuBook, Person, TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
-import { Color } from "../../Theme/Colors/Color";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { EmployeeRoutes } from "../../Routers/EmployeeRoutes";
+import { useNavigate } from 'react-router-dom';
 
-export const SideBar = ({ drawerWidth = 340 }) => {
+export const SideBar = ({drawerWidth = 340}) => {
 
-  const [disable, setDisable] = useState(false)
-    const [uuid, setuuid] = useState<any>()
-    const auth = getAuth()
-    const [loading, setLoading] = useState(false)
-    const [user2, setUser2]= useState<any>({})
-
-
-    useEffect(() => {
-        AuthCheck()
-    }, [auth])
-
-    const AuthCheck = onAuthStateChanged(auth, (user) => {
-        if (user) {
-             setuuid(user.uid) 
-            setLoading(false)
-            console.log(user.uid)
-            
-                axios({
-                    method: 'GET',
-                    url: `https://apigreendesert.onrender.com/user/one/${user.uid}`
-                }).then((res) => {
-                    console.log(res.data)
-                    setUser2(res.data)
-                    console.log(user2)
-
-                    if(user2.role.id == 1){
-                        console.log('soy operador')
-                        setDisable(true)
-                    }else{
-                        console.log('soy admin')
-                        setDisable(false)
-                    }
-                })
-            
-
-        } else {
-            
-        }
-    });
-
-  const navigate = useNavigate();
   let options = ['Inventario', 'Clientes', 'Empleados', 'Proveedores'];
-  let descriptions = ['asd', 'b', 'c', 'd'];
-
-  const changePage = (event: React.FormEvent<HTMLFormElement | HTMLButtonElement>): any => {
-    event.preventDefault();
-
-  }
-
-  
+  let descriptions = ['asd','b','c','d'];
+  const navigate = useNavigate()
 
   return (
 
     <Box
       component='nav'
-      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      sx={{ width: {sm: drawerWidth}, flexShrink: {sm: 0}}}
     >
       <Drawer
         variant="permanent"
         open
         sx={{
-          display: { xs: 'block' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+          display: {xs: 'block'},
+          '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth}
         }}
       >
         <Toolbar >
-
+          
           <Grid container
             direction='row'
             justifyContent='space-between'
             alignItems='center'
-
+          
           >
-            <Typography variant="h6" noWrap component='div'>Menu</Typography>
-
-            <IconButton>
-              <Menu />
-            </IconButton>
+          <Typography variant="h6" noWrap component='div'>Menu</Typography>
+          
+          <IconButton>
+            <Menu/>
+          </IconButton>
           </Grid>
 
-
+          
         </Toolbar>
-
-        <Divider />
+        
+        <Divider/>
         <List>
-          {
-            <ListItem key={'a'} disablePadding>
-
-              <Grid width='100%'>
-
-                <ListItemButton onClick={() => { }}>
-                  <ListItem>
-                    <Person />
-                    <ListItemText primary={'Empleados'} />
-                  </ListItem>
-                </ListItemButton>
-
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/employee/add">
-
-                      <Person />
-                      <ListItemText secondary={'Agregar empleados'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/employee">
-
-                      <Person />
-
-                      <ListItemText secondary={'Visualizar empleados'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton  onClick={() => { }}>
-                  <ListItem>
-                    <Hail />
-                    <ListItemText primary={'Clientes'} />
-                  </ListItem>
-                </ListItemButton>
-
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/customer/add">
-
-                      <Hail />
-                      <ListItemText secondary={'Agregar cliente'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/customer">
-
-                      <Hail />
-                      <ListItemText secondary={'Visualizar clientes'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton onClick={() => { }}>
-                  <ListItem>
-                    <Inventory />
-                    <ListItemText primary={'Inventario'} />
-                  </ListItem>
-                </ListItemButton>
-
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/invetory">
-                      <Inventory />
-                      <ListItemText secondary={'Visualizar inventario'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton disabled={disable} onClick={() => {  }}>
-                  <ListItem>
-                    <LocalShipping />
-                    <ListItemText primary={'Provedores'} />
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/proveedores/add">
-
-                    <LocalShipping />
-                    <ListItemText secondary={'Agregar proveedor'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton disabled={disable} onClick={() => {  }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/proveedores">
-
-                    <LocalShipping />
-                    <ListItemText secondary={'Visualizar provedores'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton onClick={() => { }}>
-                  <ListItem>
-                    <Category />
-                    <ListItemText primary={'Productos'} />
-                  </ListItem>
-                </ListItemButton>
-
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/productos">
-
-                    <Category />
-                    <ListItemText secondary={'Visualizar productos'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
-
-                <ListItemButton  onClick={() => { }}>
-                  <ListItem>
-                    <Category />
-                    <ListItemText primary={'JobOrder'} />
-                  </ListItem>
-                </ListItemButton>
-
-
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                    <a href="/joborder/add">
-
-                    <Category />
-                    <ListItemText secondary={'Crear Joborder'} />
-                    </a>
-                  </ListItem>
-                </ListItemButton>
+            {
+              <ListItem key={'a'} disablePadding>  
                 
-                <ListItemButton disabled={disable} onClick={() => { }}>
-                  <ListItem sx={{ ml: 2 }}>
-                  <Link to={"/joborder/"}>
+                <Grid width='100%'>
 
+                  <ListItemButton onClick={() => {}}>
+                    <ListItem>
+                      <Person/>
+                      <ListItemText primary = {'Empleados'}/>
+                    </ListItem>
+                  </ListItemButton>
 
-                    <Category />
-                    <ListItemText secondary={'Visualizar joborder'} />
-                  </Link>
+                  
+                  <ListItemButton onClick={() => {navigate('/employee/add');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Person/>
+                      <ListItemText secondary = {'Agregar empleados'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/employee/review')}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Person/>
+                      <ListItemText secondary = {'Visualizar empleados'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {alert('jalamos al 100')}}>
+                    <ListItem>
+                      <Hail/>
+                      <ListItemText primary = {'Clientes'}/>
+                    </ListItem>                    
+                  </ListItemButton>
+
+                  
+                  <ListItemButton onClick={() => {navigate('costumer/add');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Hail/>
+                      <ListItemText secondary = {'Agregar cliente'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/costumer/review');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Hail/>
+                      <ListItemText secondary = {'Visualizar clientes'}/>
+                    </ListItem>  
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {alert('jalamos al 100');}}>
+                    <ListItem>
+                      <Inventory/>
+                      <ListItemText primary = {'Inventario'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/inventory/add');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Inventory/>
+                      <ListItemText secondary = {'Registrar entrada'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/inventory/review');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Inventory/>
+                      <ListItemText secondary = {'Visualizar inventario'}/>
+                    </ListItem>
+                  </ListItemButton>
+                  
+                  <ListItemButton onClick={() => {alert('tamos jalando al 100');}}>
+                    <ListItem>
+                      <LocalShipping/>
+                      <ListItemText primary = {'Provedores'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/provider/add');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <LocalShipping/>
+                      <ListItemText secondary = {'Agregar proveedor'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/provider/review');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <LocalShipping/>
+                      <ListItemText secondary = {'Visualizar provedores'}/>
+                    </ListItem>
+                  </ListItemButton>
+                  
+                  <ListItemButton onClick={() => {alert('tamos jalando al 100');}}>
+                    <ListItem>
+                      <Category/>
+                      <ListItemText primary = {'Productos'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/product/add');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Category/>
+                      <ListItemText secondary = {'Agregar producto'}/>
+                    </ListItem>
+                  </ListItemButton>
+
+                  <ListItemButton onClick={() => {navigate('/product/review');}}>
+                    <ListItem sx={{ml: 2}}>
+                      <Category/>
+                      <ListItemText secondary = {'Visualizar productos'}/>
+                    </ListItem>
+                  </ListItemButton>
                     
-                  </ListItem>
-                </ListItemButton>
+                </Grid>
 
-              </Grid>
-
-            </ListItem>
-          }
-        </List>
+              </ListItem>
+            }
+          </List>
       </Drawer>
     </Box>
   )
